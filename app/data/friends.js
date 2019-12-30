a4e35217ccb6ccec29a30079e40d2d4c413731e4
Paola L <paolalarrazabal@gmail.com>
2019-12-30 18:45:14 -0500

    added modal to html and submit button, added click event for subimt button and saved values inot newUser object added ajax call for dispalying results.

--- app/data/friends.js
+++ app/data/friends.js
@@ -37,5 +37,21 @@ let scoresArray =
 }
 ]
 
-
 module.exports = scoresArray; 
+
+
+
+
+function runTableQuery() {
+    let currentURL = window.location.origin; 
+    $.ajax({url: currentURL + "/api/friends", method:"GET"})
+    .done(function(scoresArray){
+        console.log("-----------------------");
+        console.log("URL:" + currentURL + "/api/friends"); 
+        console.log("------------------------"); 
+        console.log(scoresArray); 
+        console.log("-------------------------")
+    });
+}
+
+runTableQuery(); 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,5 +5,9 @@ module.exports = function(app) {
         res.json(friendData); 
     });
 
+    app.post('/api/friends', function (req,res) {
+
+    });
+
    
 }
