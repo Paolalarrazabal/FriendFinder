20d054ee3833ea8585cbd72975bfac5446600acf
Paola L <paolalarrazabal@gmail.com>
2019-12-31 16:13:18 -0500

    switched to bootstrap framework

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,7 +2,7 @@ let scoresArray =
     [
     {
     name: "Bob",
-    photo: "Man", 
+    photo: "https://pbs.twimg.com/media/BsrkPu2CUAEWSqY.jpg", 
     scores: [
         5,
         1,
@@ -20,10 +20,10 @@ let scoresArray =
 ,
 {
     name: "Bill",
-    photo: "blob", 
+    photo: "https://pbs.twimg.com/media/BsrkPu2CUAEWSqY.jpg", 
     scores: [
-        "5",
-        "1",
+        1,
+        4,
         4,
         4,
         5,
@@ -42,16 +42,3 @@ module.exports = scoresArray;
 
 
 
-function runTableQuery() {
-    let currentURL = window.location.origin; 
-    $.ajax({url: currentURL + "/api/friends", method:"GET"})
-    .done(function(scoresArray){
-        console.log("-----------------------");
-        console.log("URL:" + currentURL + "/api/friends"); 
-        console.log("------------------------"); 
-        console.log(scoresArray); 
-        console.log("-------------------------")
-    });
-}
-
-runTableQuery(); 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,7 +7,47 @@ module.exports = function(app) {
 
     app.post('/api/friends', function (req,res) {
 
-    });
+    let userScore = req.body.scores;
+    const friendsArr = [];
+    let friendMatch = 0;
+
+    for (let i=0; i < userScore.length; i ++) {
+        friendsArr.push(parseInt(userScore[i]))
+
+    }
+
+    userScore = friendsArr;
+
+    for (let i=0; i < friendsData.length; i++) {
+        scoreDifference = 0; 
+        for (let j = 0; j < userScore.length; j++) {
+         scoreDifference += Math.abs(friendData[i].scores[j]-userScore[j]); 
+        }
+        friendsArr.push(scoreDifference); 
+
+    }
+
+    for (let i = 0; i < friendsArr.length; i ++) {
+        if (friendsArr[i] <= friendsArr[friendMatch]) {
+            friendMatch = i;
+        }
+        
+    }
+
+
+let bestMatch = friendData[friendMatch]; 
+res.json(bestMatch); 
+friendData.push(req.body); 
+console.log(req.body);
+
+
+
+
+    
+    
+    
 
    
-}
+});
+
+};
